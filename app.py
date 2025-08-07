e6a47e061b17279046f379ae412d36066feae858
TH Chew <teonghan@Abus-Macbook.local>
2025-08-07 13:19:07 +0800

    matplotlib

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 import plotly.graph_objects as go
 import plotly.express as px
